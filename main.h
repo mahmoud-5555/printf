d2028e277af66c5eaaf28c6b4508babcb77685ad
AbeerAbdo17 <abeerabdoadam3@gmail.com>
2023-10-15 04:23:40 -0700

    printf task

--- main.h
+++ main.h
@@ -8,6 +8,7 @@
 
 int _printf(const char *format, ...);
 int printint(va_list args);
+int p_bin(va_list dnum);
 void _putchar(char);
 void p_buffer(char list[], int *b);
 int *_handl_theformat(char *);
