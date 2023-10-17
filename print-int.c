c5d4032fab138d4aa8940c60ff7b0a834912b95f
AbeerAbdo17 <abeerabdoadam3@gmail.com>
2023-10-16 21:49:32 -0700

    pp

--- print-int.c
+++ print-int.c
@@ -12,29 +12,29 @@ int printint(va_list args)
 	long int n = va_arg(args, long int);
 	char buffer[BUFFER_SIZE];
 	int count = 0, x = 0, y;
-	char tmp;
+	char tmp[30];
 
 	if (n < 0)
 	{
 		buffer[count++] = '-';
 		n = -n;
 	}
-	while (n > 0)
-	{
-		buffer[count++] = (char)((n % 10) + '0');
+	do {
+		tmp[x++] = (n % 10) + '0';
 		n = n / 10;
-	}
+		count++;
+	} while (n != 0);
+
 	if (count == 0)
 	{
 		buffer[count++] = '0';
 	}
-	y = count - 1;
-	while (x < y)
+
+	y = x - 1;
+
+	while (y >= 0)
 	{
-		tmp = buffer[x];
-		buffer[x] = buffer[y];
-		buffer[y] = tmp;
-		x++;
+		buffer[count++] = tmp[y];
 		y--;
 	}
 	write(1, buffer, count);
