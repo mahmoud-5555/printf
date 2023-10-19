13dcc23bcf004676f2846b3096f0bc9cd8e52778
AbeerAbdo17 <abeerabdoadam3@gmail.com>
2023-10-19 11:21:30 -0700

    z

--- p_bin.c
+++ p_bin.c
@@ -9,27 +9,17 @@
  */
 int p_bin(va_list *args)
 {
-	int gg = 0, printed = 0, x = 0, t = 1, e;
 	unsigned int n = va_arg(*args, unsigned int);
-	unsigned int q;
+	unsigned int bno = 0;
+	unsigned int remainder, f = 1;
 
-	while (x < 32)
+	while (n != 0)
 	{
-		q = ((t << (32 - x)) & n);
-		if (q >> (31 - x))
-			gg = 1;
-		if (gg)
-		{
-			e = q >> (31 - x);
-			_putchar(e + '0');
-			printed++;
-		}
-		x++;
+		remainder = n % 2;
+		bno = bno + remainder * f;
+		f = f * 10;
+		n = n / 2;
 	}
-	if (printed == 0)
-	{
-		printed++;
-		_putchar('0');
-	}
-	return (printed);
+
+	return (bno);
 }
