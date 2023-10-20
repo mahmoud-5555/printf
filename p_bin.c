32caac16dbbde79b6ee4366ed0fcf5aa6dad01a8
AbeerAbdo17 <abeerabdoadam3@gmail.com>
2023-10-19 23:31:42 -0700

    y

--- p_bin.c
+++ p_bin.c
@@ -1,5 +1,26 @@
 #include"main.h"
+/**
+ * print_bin - print binery
+ *
+ * @n: number
+ *
+ * Return: count
+ */
+int print_bin(unsigned int n)
+{
+	int counter = 0;
 
+	if (n != 0)
+	{
+		if ((n / 2) != 0)
+			counter += print_num(n / 2);
+
+		_putchar('0' + (n % 2));
+		counter++;
+	}
+
+	return (counter);
+}
 /**
  * p_bin - convert decimal to binary
  *
@@ -9,8 +30,18 @@
  */
 int p_bin(va_list *args)
 {
-	unsigned int num = va_arg(*args, unsigned int);
+	int n = va_arg(*args, int);
 	int counter = 0;
+	unsigned int num;
+
+	if (n < 0)
+	{
+		num = -n;
+	}
+	else
+	{
+		num = n;
+	}
 
 	if (num == 0)
 	{
@@ -18,14 +49,15 @@ int p_bin(va_list *args)
 		return (1);
 	}
 
-	if (num != 0)
+	if (num > 0)
 	{
 		if ((num / 2) != 0)
-			counter += p_bin(num / 2);
+			counter += print_bin(num / 2);
 
 		_putchar('0' + (num % 2));
 		counter++;
 	}
 
 	return (counter);
+
 }
